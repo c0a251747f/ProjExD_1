df2ea476a4b2f8cd63b44f86167986bf3005f5fc
Tsubaki <c0a251747@edu.teu.ac.jp>
2026-04-14 16:20:34 +0900

    10までall

--- pygame_image.py
+++ pygame_image.py
@@ -9,24 +9,35 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg2_img = pg.image.load("fig/pg_bg.jpg")
-    bg2_img = pg.image.load(bg2_img, True, False)
-    kk_img = pg.image.load("fig/3.png")
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 練習1
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8
+    kk_img = pg.image.load("fig/3.png")  # 練習3
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # 練習10：こうかとんRectの取得
+    kk_rct.center = 300, 200  # 練習10：こうかとんの初期座標の設定
     tmr = 0
-    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+        key_lst = pg.key.get_pressed()  # 練習10：すべてのキーの押下状態の取得
+        # print(key_lst)
+        if key_lst[pg.K_UP]:  # 上矢印キーが押されていたら
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されていたら
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されていたら
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されていたら
+            kk_rct.move_ip(+1, 0)
+            
+        x= tmr%3200
+
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg2_img, [-x+1600, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
-        x += 1 
-        tmr += 1       
+        tmr += 1    
         clock.tick(200)
 
 
